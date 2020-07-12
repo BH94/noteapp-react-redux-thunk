4ff2180892c69cebcac0feccb14b5d63ba623073
BH94 <ybh942002@gmail.com>
2020-07-13 05:33:47 +0900

    :pencil: README 를 위한 image 업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-import axios from "axios";
 
 // import ClassContainer from "./containers/ClassContainer";
 import FunctionalContainer from "./containers/FunctionalContainer";
 import WeatherContainer from "./containers/WeatherContainer";
 
 const App = () => {
-  useEffect(() => {
-    //추가
-    axios
-      .get(
-        "http://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&APPID=e75a0a68adc50371c5898d8d43931062"
-      )
-      .then((response) => {
-        console.log(response.data);
-      });
-  }, []);
-
   return (
     <div>
       <WeatherContainer />
